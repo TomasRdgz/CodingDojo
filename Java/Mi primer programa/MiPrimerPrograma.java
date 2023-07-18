28ad130fb91ae6c2b10242ee0d5fd0abd69d8d91
Tomas <tomasagustinmy@gmail.com>
2023-07-18 19:16:24 -0300

    Mi primer programa

---
+++ Java/Mi primer programa/MiPrimerPrograma.java
@@ -0,0 +1,8 @@
+public class MiPrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Coding Dojo");
+        System.out.println("Tengo 100 años de edad.");
+        System.out.println("Mi ciudad es Brubank, CA");
+
+    }
+}
