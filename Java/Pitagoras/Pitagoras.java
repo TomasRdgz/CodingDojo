4e2d4f2e8410da9a459af5aa072fdd843d76f428
Tomas <tomasagustinmy@gmail.com>
2023-07-20 20:24:27 -0300

    Ultimo trabajo Pitagoras

---
+++ Java/Pitagoras/Pitagoras.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class Pitagoras {
+    public double calcularHipotenusa() {
+        double CatetoA = 4;
+        double CatetoB = 5;
+        Double Hipotenusa = Math.sqrt(Math.pow(CatetoA, 2) + Math.pow(CatetoB, 2));
+        return Hipotenusa;
+    
+} 
+}
---
+++ Java/Pitagoras/PitagorasTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest {
+public static void main(String[] args) {
+            Pitagoras Val=new Pitagoras();
+            double Calculo = Val.calcularHipotenusa();
+            System.out.println("el valor de la hipotenusa es " + Calculo);
+    }
+}
